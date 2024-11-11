e6d7163ae4276ca4207d89f31c343abeeff97890
Tony Burns <tony@tonyburns.net>
2024-11-11 11:12:14 -0500

    fix: fix constant

--- lib/rubocop/picket.rb
+++ lib/rubocop/picket.rb
@@ -4,8 +4,8 @@ require_relative "picket/version"
 
 module RuboCop
   module Picket
-    PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
+    PROJECT_ROOT = Pathname.new(__dir__).parent.parent.expand_path.freeze
     CONFIG_DEFAULT = PROJECT_ROOT.join("config", "default_cops.yml").freeze
-    CONIG_RAILS = PROJECT_ROOT.join("config", "rails_cops.yml").freeze
+    CONFIG_RAILS = PROJECT_ROOT.join("config", "rails_cops.yml").freeze
   end
 end
